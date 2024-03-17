7cf855ed8b5886d6fd1c8c0f243fac09589be4b5
KashiwaByte <471314513@qq.com>
2024-03-17 23:57:03 +0800

    feat:basic structure

--- Dagent/AbLLM.py
+++ Dagent/AbLLM.py
@@ -2,3 +2,15 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from random import randrange
 from typing import List
+
+
+class abllm(ABC):
+    
+    def __init__():
+        pass
+    
+    
+    @abstractmethod
+    def invoke():
+        pass
+    
