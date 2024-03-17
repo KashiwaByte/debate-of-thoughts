c21f5ec94a6728e824b510f06bf99a458dbcb596
KashiwaByte <471314513@qq.com>
2024-03-17 23:53:08 +0800

    feat:add func

--- Dnode/AbNode.py
+++ Dnode/AbNode.py
@@ -8,15 +8,28 @@ class AbNode(ABC):
     
     
     
-    def __init__():
+    def __init__(self,round:int,out_node:AbNode):
+        self.round_id = round
+        self.target = out_node
         pass
         
         
     
     
     @abstractmethod
-    def get_content():
+    def get_content(self,out_node):
+        ''' self.content = func(out_node.content) '''
+       
         pass
+    
+    
+    def get_point(self):
+        pass
+    
+    def get_depth(self,target):
+        self.depth = target.depth + 1
+        pass
+    
     
     
     
