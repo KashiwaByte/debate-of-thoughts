fd749c6972fa6ff4c11848b958f69bac8ec8da9b
黄柏特 <471314513@qq.com>
2024-03-21 23:05:55 +0800

    feat:add upload and test

---
+++ Dgraph/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+r"""
+@DATE: 2024-03-21 21:38:02
+@File: Dgraph\__init__.py
+@IDE: vscode
+@Description:
+    图数据库模块
+"""
+from .upload import node_upload
--- Dgraph/upload.py
+++ Dgraph/upload.py
@@ -1,5 +1,27 @@
-from neo4j import GraphDatabase
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+r"""
+@DATE: 2024-03-21 21:37:27
+@File: Dgraph\upload.py
+@IDE: vscode
+@Description:
+    数据上传Neo4j函数
+"""
+from py2neo import Graph, Node, Relationship
 
 # 建立一个Neo4j会话
-driver = GraphDatabase.driver('bolt://localhost:7687', auth=('neo4j', 'NEO123456neo'))
 
+
+def node_upload(max_round,node_list):
+    graph = Graph("bolt://localhost:7687", auth=("neo4j", "NEO123456neo"))
+    var = "upload"
+    node_name = f'{var}1' 
+    globals()[node_name] = Node("round",**node_list[0])
+ 
+    for i in (2,max_round+1):
+        node_name = f'{var}{i}' 
+        globals()[node_name] = Node("round",**node_list[i-1])
+        relationship = Relationship(globals()[node_name],node_list[i-1]["stand"],globals()[f'upload{globals()[node_name]["target"].round_id}'])
+        graph.create(relationship)
+
+ 
