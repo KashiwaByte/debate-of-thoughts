77e9c894256e724ce592d741ee4be0f5d8c7cfd2
黄柏特 <471314513@qq.com>
2024-03-22 19:28:32 +0800

    fix: graphupload and test

--- Dgraph/upload.py
+++ Dgraph/upload.py
@@ -13,15 +13,32 @@ from py2neo import Graph, Node, Relationship
 
 
 def node_upload(max_round,node_list):
+    """传入的node_list的修改target类型后上传到图数据库"""
+    node_lists =  node_list
+    print(f"初始{node_lists}")
+    
+    for i in range(0,max_round):
+        """从第一项遍历到最后一项"""
+        node_lists[i]["node"] = f'graphnode{node_lists[i]["round_id"]}'
+        node_lists[i]["debater"] = f'{node_lists[i]["debater"].__class__}'
+    
+    for i in range(1,max_round):
+        """从第二项遍历到最后一项"""
+        node_lists[i]["target"] = f'graphnode{node_lists[i]["target"].round_id}'
+    print(f"修改后:{node_lists}")
     graph = Graph("bolt://localhost:7687", auth=("neo4j", "NEO123456neo"))
-    var = "upload"
+    var = "graphnode"
     node_name = f'{var}1' 
-    globals()[node_name] = Node("round",**node_list[0])
+    globals()[node_name] = Node("round",**node_lists[0])
  
-    for i in (2,max_round+1):
+    for i in range(2,max_round+1):
         node_name = f'{var}{i}' 
         globals()[node_name] = Node("round",**node_list[i-1])
-        relationship = Relationship(globals()[node_name],node_list[i-1]["stand"],globals()[f'upload{globals()[node_name]["target"].round_id}'])
+        relationship = Relationship(globals()[node_name],node_lists[i-1]["stand"],globals()[f'{globals()[node_name]["target"]}'])
         graph.create(relationship)
 
+ 
+ 
+ 
+ 
  
